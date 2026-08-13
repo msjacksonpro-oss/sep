5bf39f957ab426e6a9d5c1781cd2b9c38f8e5ba5
JACKSON <ms.jackson.pro@gmail.com>
2026-08-13 09:40:48 +0530

    jacky

--- UniversityHub/UniversityHub/urls.py
+++ UniversityHub/UniversityHub/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authentication.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('academic.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
